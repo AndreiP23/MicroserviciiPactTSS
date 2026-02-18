1715d4aad8050ed6d13a390bc689cfc478680535
AndreiP23 <66410327+AndreiP23@users.noreply.github.com>
2026-02-18 20:50:48 +0200

    Important for another project

--- provider/SampleProviderTests/UnitTest1.cs
+++ provider/SampleProviderTests/UnitTest1.cs
@@ -15,20 +15,21 @@ public class UserProviderTests : IClassFixture<WebApplicationFactory<Program>>
     [Fact]
     public void VerifyPactWithConsumer()
     {
-        // 1. Definim calea catre contractul generat de Consumer
+        //  Definim calea catre contractul generat de Consumer
         var pactPath = Path.Combine("..", "..", "..", "..", "..", "consumer", "SampleConsumerTests", "pacts", "FrontendApp-UserAPI.json");
 
         var config = new PactVerifierConfig();
 
-        // 2. Cream un Verifier care va citi JSON-ul si va lovi API-ul real
+        //  Cream un Verifier care va citi JSON-ul si va lovi API-ul real
         var verifier = new PactVerifier("UserAPI", config);
 
-        // 3. Adresa unde ruleaza API-ul tau (verifica portul cand dai F5)
+        //  Adresa unde ruleaza API-ul
         var providerUri = new Uri("http://localhost:5223");
 
         verifier
             .WithHttpEndpoint(providerUri)
             .WithFileSource(new FileInfo(pactPath))
-            .Verify(); // Compara raspunsul API-ului cu cel din JSON
+            .Verify(); 
+        // Compara raspunsul API-ului cu cel din JSON
     }
 }
